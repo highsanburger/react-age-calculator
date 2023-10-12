230bb1566353d5306c760f4adcabb23a28920b31
Syed Khalid <magmastar167@gmail.com>
2023-10-12 11:59:18 +0300

    🌛

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [birthDate, setBirthDate] = useState('');
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    if (birthDate) {
+      const birthYear = new Date(birthDate).getFullYear();
+      const currentYear = new Date().getFullYear();
+      const calculatedAge = currentYear - birthYear;
+      setAge(calculatedAge);
+    } else {
+      setAge(null);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Calculator</h1>
+      <form>
+        <label htmlFor="birthDate">Enter your Date of Birth:</label>
+        <input
+          type="date"
+          id="birthDate"
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+        />
+        <button type="button" onClick={calculateAge}>
+          Calculate Age
+        </button>
+      </form>
+      {age !== null && <p>You are {age} years old.</p>}
     </div>
   );
 }
 
 export default App;
+
